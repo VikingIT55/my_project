2ac70be7a9fdd11e744861739496c6a57f748f07
VikingIT55 <foxkarasava55@gmail.com>
2023-02-28 18:33:40 +0100

    added function run_spotify_etl

---
+++ dags/spotify_dag.py
@@ -0,0 +1,30 @@
+from datetime import timedelta, datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+from Spotify_ETL import run_spotify_etl
+
+default_args = {
+    'owner': 'airflow',
+    'depens_on_past': False,
+    'start_date': datetime(2023, 2, 28),
+    'email': ['foxkarasava55@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'spotify_dag',
+    default_args=default_args,
+    description='DAG with ETL process',
+    schedule_interval=timedelta(days=1)
+)
+
+run_etl = PythonOperator(
+    task_id='whole_spotify_etl',
+    python_callable=run_spotify_etl,
+    dag=dag
+)
+run_etl
